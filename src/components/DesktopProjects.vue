b992cddda6da9b25362bb9af7e778763f1deef6e
Gerald Ho <97504394+Geraldworks@users.noreply.github.com>
2023-07-16 00:51:36 +0800

    abstracted project info

--- src/components/DesktopProjects.vue
+++ src/components/DesktopProjects.vue
@@ -1,68 +1,11 @@
 <script>
 import TimelineItem from "./TimelineItem.vue";
+import projectInfo from "./projectInfo"
 
 export default {
   data() {
     return {
-      website: {
-        srcImg: "/images/website.png",
-        projectTitle: "Personal Website",
-        projectDuration: "June 2023 - July 2023",
-        githubLink: "https://github.com/Geraldworks/Website",
-        techStack: ["Vue", "Vuetify", "Vuex", "Vue-Router"],
-        // personal
-      },
-      gymmbuddy: {
-        srcImg: "/images/gymmbuddy.png",
-        projectTitle: "GymmBuddy",
-        projectDuration: "Jan 2023 - Apr 2023",
-        projectLink: "https://gymmbuddy-3e930.web.app/",
-        githubLink: "https://github.com/Geraldworks/BT3103",
-        techStack: ["Vue", "Vuex", "Vue-Router", "Firestore"],
-        // bt3103
-      },
-      resumeProcessor: {
-        srcImg: "/images/rejected.jpg",
-        projectTitle: "Fit for the Job?",
-        projectDuration: "Jan 2023 - Apr 2023",
-        githubLink: "https://github.com/Geraldworks/BT4222",
-        techStack: ["Web Scraping", "Pandas", "scikit-learn", "Tensorflow 2"],
-        // bt4222
-      },
-      hmm: {
-        srcImg: "/images/hmm.png",
-        projectTitle: "POS Tagging",
-        projectDuration: "Jan 2023 - Apr 2023",
-        techStack: ["Python"],
-        // bt3102
-      },
-      analysis: {
-        srcImg: "/images/credit.jpg",
-        projectTitle: "Credit Defaulter",
-        projectDuration: "Jan 2023 - Apr 2023",
-        projectLink:
-          "https://www.kaggle.com/datasets/uciml/default-of-credit-card-clients-dataset",
-        githubLink: "https://github.com/Geraldworks/BT2103",
-        techStack: ["Pandas", "Seaborn", "scikit-learn", "Tensorflow 2"],
-        // bt2103
-      },
-      orbital: {
-        srcImg: "/images/robot.png",
-        projectTitle: "RobotInvazion",
-        projectDuration: "May 2022 - Aug 2022",
-        projectLink: "https://www.youtube.com/watch?v=A3uXztc71QU",
-        githubLink: "https://github.com/Geraldworks/RobotInvazion",
-        techStack: ["C#", "Unity"],
-        // orbital
-      },
-      database: {
-        srcImg: "/images/library.jpeg",
-        projectTitle: "Library System",
-        projectDuration: "Jan 2022 - Apr 2022",
-        githubLink: "https://github.com/Geraldworks/BT2102",
-        techStack: ["MySQL", "tkinter"],
-        // bt2102
-      },
+      ...projectInfo
     };
   },
   components: {
